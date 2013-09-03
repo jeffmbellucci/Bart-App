a35ac61ffdcc23d156e27ac991c9c419dee16f10
Jeff Bellucci <jeffmbellucci@gmail.com>
2013-09-03 14:27:09 -0700

    station controller now returns json, added station js

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -4,10 +4,15 @@ class StationsController < ApplicationController
   
   def index
     @stations = Station.all
+    render :index
   end
   
   def show
     @station = Station.find(params[:id])
+    respond_to do |format| 
+      format.html { render :show }
+      format.json { render json: get_station_data(@station.abbr) }
+    end
   end
   
 end
