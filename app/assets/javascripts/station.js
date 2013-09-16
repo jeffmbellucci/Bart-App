5f93ad1e2796611bc78e22f8b198c5f1499c4714
Jeff Bellucci <jeffmbellucci@gmail.com>
2013-09-16 01:57:59 -0700

    created toggle link for times format, seems to work fine

--- app/assets/javascripts/station.js
+++ app/assets/javascripts/station.js
@@ -13,8 +13,8 @@
 			var spinnerView = JST["spinner_template"]();
 			//add spinner
 			$(spinnerView).hide().prependTo($($link).parent()).fadeIn(200);
-			 addCloseListener();
-		 
+			addCloseListener();
+		 	
 			getStationData($link.id, function(data){
 			
 				var stationTemplateFn = JST["station_template"];
@@ -24,7 +24,9 @@
 				closeSpinner();
 			
 				$(timesView).prependTo($($link).parent());
+				addToggleListener();
 				addCloseListener();
+				
 			});			
 		});
 	};
@@ -41,14 +43,51 @@
 		$(document).ready(function() {
 			$('.closeLink').on('click', closeAllTimes);	
 		});
+	};
+	
+	var addToggleListener = BA.addToggleListener = function () {
+		$(document).ready(function() {
+			$('.toggleLink').on('click', toggleTimes);	
+		});
 	}
 
 	var closeAllTimes = BA.closeAllTimes = function () {
 		$('.bubble').fadeOut(200); // versus hide() ???
 	};
+	
+	var toggleTimes = BA.toggleTimes = function () {
+		$('.timesContainer').toggleClass("hidden");
+		$('.minutesContainer').toggleClass("hidden");
+	};
 
 	var closeSpinner = BA.closeSpinner = function () {
 		$('.spinner').hide();
+	}
+	
+	var addMinutes = BA.addMinutes = function(date, minutes) {
+		var newDate = new Date(date)
+		newDate.setMinutes(date.getMinutes() + minutes)
+		return newDate
+	}
+	
+	var printTime = BA.printTime = function(date) {
+		var result = new Array();
+		if (date.getHours() > 12) {
+		    result[0] = date.getHours() - 12;
+		} else if (date.getHours() == 0 ) {
+		    result[0] = "12";
+		} else {
+		    result[0] = date.getHours();
+		}
+		result[1] = ":"
+		result[2] = date.getMinutes();
+
+		if (date.getHours() > 12) {
+		    result[3] = "pm";
+		} else {
+		    result[3] = "am";
+		}
+		return result.join("");
 	}
 	
 })();
