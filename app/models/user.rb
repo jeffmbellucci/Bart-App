dc1008829ac741731da04cc6e13aa6e831226d9e
Jeff Bellucci <jeffmbellucci@gmail.com>
2013-10-02 02:33:25 -0700

    added presence requirement for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ActiveRecord::Base
                            
   VALID_EMAIL_REGEX = /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
   validates :email, format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true
+                    uniqueness: true,
+                    presence: true
       
   has_many :reminders
   
