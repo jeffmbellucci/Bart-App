73f9a51828603144d55f9a93cbbeeace1f7451a3
Jeff Bellucci <jeffmbellucci@gmail.com>
2013-10-17 16:00:54 -0700

    added error message for when Bart API is down

--- app/helpers/stations_helper.rb
+++ app/helpers/stations_helper.rb
@@ -14,16 +14,16 @@ module StationsHelper
       
     response = HTTParty.get(bart_query).to_json
     json_response = JSON.parse(response)
+    
+    if json_response['root']['station']['message']['error']
+      json_response['root']['message'] = "Bart server error."
+      return json_response
+    end
    
     if json_response['root']['message']
       json_response['root']['message'] = "No trains at this time." 
       return json_response
    end
-    
-    if json_response['root']['station']['message']
-      json_response['root']['message'] = "No trains at this time."
-      return json_response
-    end
   
     unless json_response['root']['station']['etd'].is_a?(Array)
       json_response['root']['station']['etd'] = [json_response['root']['station']['etd']]
