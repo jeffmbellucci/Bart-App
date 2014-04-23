25f9ded5d1a048149afaa336dc97dba82038b347
Jeff Bellucci <jeffmbellucci@gmail.com>
2014-04-22 23:20:02 -0700

    whitespace cleanup

--- app/assets/javascripts/closest_stations.js
+++ app/assets/javascripts/closest_stations.js
@@ -1,32 +1,31 @@
 (function (root) {
 	var BA = root.BA = (root.BA || {});
-	
-	var closestStationsHandler = BA.closestStationsHandler = function () {	
+
+	var closestStationsHandler = BA.closestStationsHandler = function () {
 		addClosestStationsListener();
 	};
-	
+
 	var addClosestStationsListener = BA.addClosestStationsListener = function () {
 		$('#closest_station').on("click", function (event) {
 			event.preventDefault();
 			$('.alert').remove();
-			var alert = JST['alert_template']({data: {'alertType': 'alert alert-success', 
-													  'message': 'Calculating your closest station...'}});										  
-		    $('#wrap').append(alert);
+			var alert = JST['alert_template']({data: {'alertType': 'alert alert-success', 'message': 'Calculating your closest station...'}});
+			$('#wrap').append(alert);
 			getUserPosition();
 		});
 	};
 
 	var getUserPosition = BA.getUserPosition = function () {
-	    if (navigator.geolocation) {
-	    	navigator.geolocation.getCurrentPosition(sendPositionData);
-	    } else {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(sendPositionData);
+		} else {
 			alert("Geolocation is not supported by this browser.");
-	    };
+		};
 	};
-	
+
 	//add contains method to string
 	String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
-	
+
 	var sendPositionData = BA.sendPositionData = function (position) {
 		$.ajax({
 			type: "POST",
@@ -36,21 +35,18 @@
 				$('.alert').remove();
 				console.log(data.length);
 				if (data.length === 0) {
-					var alert = JST['alert_template']({data: {'alertType': 'alert alert-error', 
-															  'message': 'There are no BART stations in your area.'}});
+					var alert = JST['alert_template']({data: {'alertType': 'alert alert-error', 'message': 'There are no BART stations in your area.'}});
 					$('#wrap').append(alert);
 					return;
-				}	
+				}
 				var stationName = data[0]['name'];
 				console.log(data);
 				if (stationName.contains("BART") || stationName.contains("Bart") || stationName.contains("bart")) {
-					var alert = JST['alert_template']({data: {'alertType': 'alert alert-success', 
-															  'message': 'Your closest station: ' + stationName}});									  
-				    $('#wrap').append(alert);		
+					var alert = JST['alert_template']({data: {'alertType': 'alert alert-success', 'message': 'Your closest station: ' + stationName}});
+					$('#wrap').append(alert);
 				} else {
-					var alert = JST['alert_template']({data: {'alertType': 'alert alert-success', 
-															  'message': 'Best guess: ' + stationName}});
-					$('#wrap').append(alert);	
+					var alert = JST['alert_template']({data: {'alertType': 'alert alert-success', 'message': 'Best guess: ' + stationName}});
+					$('#wrap').append(alert);
 				}
 			}
 		});
