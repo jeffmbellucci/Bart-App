d3cf69ed8ebefc80c43db3406d5e1187f7412972
Jeff <jeffmbellucci@gmail.com>
2016-02-11 22:59:35 -0800

    code cleanup

--- app/helpers/stations_helper.rb
+++ app/helpers/stations_helper.rb
@@ -1,6 +1,6 @@
 module StationsHelper
   BART_API_KEY = ENV["BART_API_KEY"]
-  
+
   def get_station_data(abbr)
     bart_query = Addressable::URI.new(
       scheme: "http",
@@ -9,32 +9,32 @@ module StationsHelper
       query_values: {
         cmd: "etd",
         orig: abbr,
-        key: BART_API_KEY 
+        key: BART_API_KEY
       })
-      
+
     response = HTTParty.get(bart_query).to_json
     json_response = JSON.parse(response)
-    
+
     if json_response['root']['message']
       json_response['root']['message'] = "No trains at this time." 
       return json_response
     end
- 
+
     if json_response['root']['station']['message']
       if json_response['root']['station']['message']['error'] == "Updates are temporarily unavailable."
-        json_response['root']['message'] = "Bart is not providing updates at this time." 
+        json_response['root']['message'] = "Bart is not providing updates at this time."
         return json_response
       end
     end
-   
+
     unless json_response['root']['station']['etd'].is_a?(Array)
       json_response['root']['station']['etd'] = [json_response['root']['station']['etd']]
     end
-    
+
     lines = json_response['root']['station']['etd']
     lines.each do |line|
       line['estimate'] = [line['estimate']] unless line['estimate'].is_a?(Array)
     end
-    json_response   
+    json_response
   end
 end
