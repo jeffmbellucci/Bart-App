4fc8324f42eddb880d4338d19458d4722f244ca1
Jeff <jeffmbellucci@gmail.com>
2016-02-11 19:49:09 -0800

    much cleaner way to get the station id and pass it to the controller

--- app/assets/javascripts/station.js
+++ app/assets/javascripts/station.js
@@ -1,73 +1,74 @@
 (function (root) {
 	var BA = root.BA = (root.BA || {});
-	
+
 	var initialize = BA.initialize = function() {
-  
+
     $('#stationsMap a').on('click', function(event) {
 			event.preventDefault();
-			var $link = this;
+			var $stationLink = $(this); // The link of the station clicked on by the user
+
+			// Close all open time bubbles before opening a new one so that you can't open more than one at a time
 			closeAllTimes();
-			
-			console.log($link.id);
-		
+
 			var spinnerView = JST["spinner_template"]();
-			//add spinner
-			$(spinnerView).hide().prependTo($($link).parent()).fadeIn(200);
+			// Add spinner while waiting for Bart API response
+			$(spinnerView).hide().prependTo($stationLink.parent()).fadeIn(200);
 			addCloseListener();
-		 	
-			getStationData($link.id.substring(8, $link.id.length), function(data) {
+
+			getStationData($stationLink.data('station-id'), function(data) {
 				var stationTemplateFn = JST["station_template"];
 				var timesView = stationTemplateFn({data: data});
-			
-				// hide spinner
+
+				// Hide spinner after successful response from Bart API
 				closeSpinner();
-			
-				$(timesView).prependTo($($link).parent());
+
+				$(timesView).prependTo($stationLink.parent());
 				addCloseListener();
 				addToggleListener();
-			});				
+			});
 		});
 	};
-	
+
 	var getStationData = BA.getStationData = function (id, callback) {
 		$.ajax({
 			type: "GET",
-			url: ("/stations/").concat(id).concat(".json"),
+			url: "/stations/" + id,
+			dataType: 'json',
 			success: callback
 		});
 	};
 
 	var addCloseListener = BA.addCloseListener = function () {
-		$('.closeLink').on('click', closeAllTimes);	
+		$('.closeLink').on('click', closeAllTimes);
 	};
-	
+
 	var addToggleListener = BA.addToggleListener = function () {
-		$('.toggleLink').on('click', toggleTimes);	
+		$('.toggleLink').on('click', toggleTimes);
 	}
 
 	var closeAllTimes = BA.closeAllTimes = function () {
-		$('.bubble').fadeOut(200, this.remove); // versus hide() ???
+		$('.bubble').fadeOut(200, this.remove);
 	};
-	
+
 	var toggleTimes = BA.toggleTimes = function () {
 		$('.timesContainer').toggleClass("hidden");
 		$('.minutesContainer').toggleClass("hidden");
 	};
 
 	var closeSpinner = BA.closeSpinner = function () {
-		$('.spinner').remove(); // versus hide?
+		$('.spinner').remove();
 	}
-	
+
 	var addMinutes = BA.addMinutes = function(date, minutes) {
 		var newDate = new Date(date)
 		newDate.setMinutes(date.getMinutes() + minutes);
 		return newDate;
 	};
-		
+
 	var printDate = BA.printDate = function(date) {
 		return date.toString().substring(4, 10);
 	};
-	
+
 	var printTime = BA.printTime = function(date) {
 		var output = new Array();
 		if (date.getHours() > 12) {
@@ -86,7 +87,7 @@
 		};
 		return output.join("");
 	};
-	
+
 	var printAmPm = BA.printAmPm = function (date) {
 		if (date.getHours() < 12) {
 			return "AM";
@@ -94,7 +95,7 @@
 			return "PM";
 		};
 	};
-	
+
 })(this);
 
 $(document).ready(BA.initialize);
